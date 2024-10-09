8dbe2cbb3e2585bb6bc203668d1d0ce3bc7a99df
Noxctis <chrysseansevilla@gmail.com>
2024-10-09 18:41:13 +0800

    fixed election

---
+++ MidtermExam/old election/Candidate.java
@@ -0,0 +1,45 @@
+
+public class Candidate {
+
+	   	private String surName;
+	    private int votesReceived;
+	    
+	    private static int numberTotalVotes = 0;
+	    
+	    public Candidate() {
+	        this.surName = "";
+	        this.votesReceived = 0;
+	    }
+
+	    public Candidate(String newSurname, int newNumberOfVotes) {
+	    	this.surName = newSurname;
+	        this.votesReceived = newNumberOfVotes;
+	        updateTotalNumberVotes(newNumberOfVotes);
+	    }
+
+	    public String getSurname() {
+	        return surName;
+	    }
+
+	    public void setSurname(String newSurname) {
+	        this.surName = newSurname;
+	    }
+	    
+
+	    public int getVotesReceived() {
+	        return votesReceived;
+	    }
+
+	    public void setVotesReceived(int newNumberOfVotes) { 
+	        this.votesReceived = newNumberOfVotes;
+	    }
+
+	    private static void updateTotalNumberVotes(int newNumberOfVotes) {
+	    	numberTotalVotes += newNumberOfVotes;
+	    }
+	    
+	    public static int getTotalNumberVotes() {
+	    	return numberTotalVotes;
+	    }
+	    
+}
---
+++ MidtermExam/old election/ElectionTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ElectionTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Election election1 = new Election();
+
+        //System.out.println(student5.getName() + " " + student5.getGPA() + " " + student5.getCreditAmount());
+        //student5.displayDetails();
+        
+        System.out.printf("%-30s %-20s %-15s%n", "Candidate", "Votes Received", "% of Total Votes");
+   
+        election1.displayResults();
+        System.out.println("Total" + "                          " + Candidate.getTotalNumberVotes());
+        scanner.close();
+    }
+}
